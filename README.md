46138a2108420148cd1843b29e08b6acfa8b110a
joko-dev <>
2023-11-29 22:06:07 +0100

    Init Solution

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# AdventOfCode2023
+# 🎄 AdventOfCode2023 🎄
 https://adventofcode.com/
+
+Repo contains all of the used code for solving the tasks, can include some quick hacks and lack of code cleanliness because of lazyness
+
+All projects: .NET 7
+
+## How to run?
+Each day has its own console application. Start the corresponding project and specify the input file ( [1] or [2] for predefined files, 
+otherwise any custom file. The console will prompt the result for each subtask. 
+
+## Progression
+:x::x: [Day  1](/AdventOfCode2022/Day01) \
+:x::x: [Day  2](/AdventOfCode2022/Day02) \
+:x::x: [Day  3](/AdventOfCode2022/Day03) \
+:x::x: [Day  4](/AdventOfCode2022/Day04) \
+:x::x: [Day  5](/AdventOfCode2022/Day05) \
+:x::x: [Day  6](/AdventOfCode2022/Day06) \
+:x::x: [Day  7](/AdventOfCode2022/Day07) \
+:x::x: [Day  8](/AdventOfCode2022/Day08) \
+:x::x: [Day  9](/AdventOfCode2022/Day09) \
+:x::x: [Day 10](/AdventOfCode2022/Day10) \
+:x::x: [Day 11](/AdventOfCode2022/Day11) \
+:x::x: [Day 12](/AdventOfCode2022/Day12) \
+:x::x: [Day 13](/AdventOfCode2022/Day13) \
+:x::x: [Day 14](/AdventOfCode2022/Day14) \
+:x::x: [Day 15](/AdventOfCode2022/Day15) \
+:x::x: [Day 16](/AdventOfCode2022/Day16) \
+:x::x: [Day 17](/AdventOfCode2022/Day17) \
+:x::x: [Day 18](/AdventOfCode2022/Day18) \
+:x::x: [Day 19](/AdventOfCode2022/Day19) \
+:x::x: [Day 20](/AdventOfCode2022/Day20) \
+:x::x: [Day 21](/AdventOfCode2022/Day21) \
+:x::x: [Day 22](/AdventOfCode2022/Day22) \
+:x::x: [Day 23](/AdventOfCode2022/Day23) \
+:x::x: [Day 24](/AdventOfCode2022/Day24) \
+:x::x: [Day 25](/AdventOfCode2022/Day25) \
---
+++ SharedKernel/Coordinate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.SharedKernel
+{
+    public class Coordinate
+    {
+
+        public Coordinate(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public Coordinate((int x, int y) point)
+        {
+            this.X = point.x;
+            this.Y = point.y;
+        }
+
+        public Coordinate(string coordinate)
+        {
+            string[] temp = coordinate.Split(',');
+            X = int.Parse(temp[0]);
+            Y = int.Parse(temp[1]);
+        }
+
+        public Coordinate(Coordinate coordinate) 
+        { 
+            this.X = coordinate.X;
+            this.Y = coordinate.Y;
+        }
+
+        public void Move(int xToMove, int yToMove)
+        {
+            X += xToMove;
+            Y += yToMove;
+        }
+
+        public void Move(Coordinate toMove)
+        {
+            Move(toMove.X, toMove.Y);
+        }
+
+        public bool Equals(Coordinate coordinate)
+        {
+            return (this.X == coordinate.X && this.Y == coordinate.Y);
+        }
+
+        public List<Coordinate> GetAdjacentCoordinates()
+        {
+            List<Coordinate> adjacent = new List<Coordinate>();
+            adjacent.Add(new Coordinate(this.X - 1, Y - 1));
+            adjacent.Add(new Coordinate(this.X, Y - 1));
+            adjacent.Add(new Coordinate(this.X + 1, Y - 1));
+            adjacent.Add(new Coordinate(this.X - 1, Y));
+            adjacent.Add(new Coordinate(this.X + 1, Y));
+            adjacent.Add(new Coordinate(this.X - 1, Y + 1));
+            adjacent.Add(new Coordinate(this.X, Y + 1));
+            adjacent.Add(new Coordinate(this.X + 1, Y + 1));
+
+            return adjacent;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public static explicit operator (int x, int y)(Coordinate obj)
+        {
+            return (obj.X, obj.Y);
+        }
+    }
+}
+
