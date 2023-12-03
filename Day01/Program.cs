97bfbfb67ba230251b7c1c56d2c730ec819a7958
joko-dev <>
2023-12-03 17:05:41 +0100

    Day 01 - Part 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Day01
+﻿using AdventOfCode2022.SharedKernel;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+
+namespace Day01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 01: Trebuchet?!"));
+            Console.WriteLine("Calibration document: ");
+            PuzzleInput puzzleInput = new(PuzzleOutputFormatter.getPuzzleFilePath(), true);
+
+            int sum = 0;
+
+            foreach(string line in puzzleInput.Lines)
+            {
+                List<char> digits = line.Where(c => Char.IsDigit(c)).ToList();
+                string number = String.Concat(digits.First(), digits.Last());
+
+                sum += int.Parse(number) ;
+            }
+
+            Console.WriteLine("Sum calibration values: {0}", sum);
         }
     }
 }
